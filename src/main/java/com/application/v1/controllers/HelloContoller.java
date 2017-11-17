a0e928dadbd6942c8a19d8ece2ed2988d30b6b28
xiaoyu <1264642534@qq.com>
2017-11-17 17:03:12 +0800

    totalpage

--- src/main/java/com/application/v1/controllers/HelloContoller.java
+++ src/main/java/com/application/v1/controllers/HelloContoller.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,7 @@ public class HelloContoller {
         view.put("main", "true");
         List<User> userList = userDao.findAll();
         view.put("colletion", userList);
+        view.put("total", userList.size());
         return "/v1/admin/user/index";
     }
 
